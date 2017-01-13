3ea0d6a8585fcd1939e2e6a26bea5e179873f200
jprittie <eyewallop@gmail.com>
2017-01-13 12:19:54 -0500

    Adds salt

--- src/models/users.js
+++ src/models/users.js
@@ -15,20 +15,29 @@ var UserSchema = new mongoose.Schema({
   },
   hashedPassword: {
     type: String,
-    required: true
+    // required: true
   }
 
 });
 
+var saltRounds = 10;
+UserSchema.methods.setPassword = function (password) {
+  // Update the User model to store the user's password as a hashed value.
+  var salt = bcrypt.genSaltSync(saltRounds);
+  this.hashedPassword = bcrypt.hashSync(password, salt);
+};
+
+// I haven't salted password, but is that in requirements?
 // hash password before saving to database
-UserSchema.pre('save', function(next){
-  var user = this;
-  bcrypt.hash(user.password, 10, function(err, hash){
-    if (err) return next(err);
-    user.hashedPassword = hash;
-    next();
-  });
-});
+// UserSchema.pre('save', function(next){
+//   var user = this;
+//   bcrypt.hash(user.password, 10, function(err, hash){
+//     if (err) return next(err);
+//     user.hashedPassword = hash;
+//     console.log(user.hashedPassword);
+//     next();
+//   });
+// });
 
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
