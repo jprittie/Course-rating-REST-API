509773cacd9278e58e74b8ef13515a6f822a81be
jprittie <eyewallop@gmail.com>
2017-01-14 11:19:11 -0500

    Adds authorization

--- src/auth.js
+++ src/auth.js
@@ -1,1 +1,46 @@
 'use strict';
+
+var User = require('./models/users');
+var basicAuth = require('basic-auth');
+var bcrypt = require('bcrypt');
+
+
+
+var auth = function (req, res, next){
+
+
+  // parse the Authorization header credentials
+  var user = basicAuth(req);
+
+  // try this without function - might be clearer how errors are handled
+  // could also send validation message for user
+  function unauthorised (res) {
+    return res.send(401);
+  }
+
+  // if the user, user.name or user.pass don't exist
+  if (!user || !user.name || !user.pass) {
+    // return unauthorised
+    return unauthorised(res);
+  } else {
+
+    // maybe would be clearer if you called email parameter "result"
+    User.findOne({emailAddress: user.name}, function (err, email) {
+      if (err) return next(err);
+      if (email) {
+        if (bcrypt.compareSync(user.pass, email.hashedPassword)) {
+          req.user = email;
+          return next();
+        } else {
+          return unauthorised(res);
+        }
+      } else {
+        // if user isn't in database
+        return unauthorised(res);
+      }
+    });
+
+  }
+};
+
+module.exports = auth;
