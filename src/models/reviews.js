fbe8c6c83be5c7998db983947d22185ed2de4115
jprittie <eyewallop@gmail.com>
2017-01-20 18:21:26 -0500

    Adds details to review model

--- src/models/reviews.js
+++ src/models/reviews.js
@@ -1,16 +1,31 @@
 'use strict';
 var mongoose = require('mongoose');
+// Patrick used integer validator here
+
+// not sure I need the following line
+var User = require('./users');
 
 var ReviewSchema = new mongoose.Schema({
-  // _id (ObjectId, auto-generated)
-  // user (_id from the users collection)
 
-  postedOn: Date,
-  rating: Number,
-  review: String
+  user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    postedOn: {
+      type: Date,
+      default: Date.now
+    },
+    rating: {
+      type: Number,
+      required: [true, 'Please enter a rating'],
+      min: 1,
+      max: 5,
+      default: 0,
+      // integer: 'Rating must be an integer.'
+    },
+    review: String
 
 });
-
 
 
 var Review = mongoose.model('Review', ReviewSchema);
