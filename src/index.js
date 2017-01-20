3dbd5cb233051f3fb75cd1d65dc58acfcd42d0a5
jprittie <eyewallop@gmail.com>
2017-01-20 14:13:35 -0500

    Edits comments

--- src/index.js
+++ src/index.js
@@ -11,41 +11,36 @@ var jsonParser = require('body-parser').json;
 require('./models/courses');
 require('./models/reviews');
 require('./models/users');
-// Require database
+// Require file that opens mongoose connection and seeds database
 require('./database');
-// Require routes
-var routes = require('./routes');
-// require('./routes/courses');
-// require('./routes/reviews');
-// require('./routes/users');
 
 
-// set port to 5000
+// Set port to 5000
 app.set('port', process.env.PORT || 5000);
 
-// morgan gives us http request logging
+// Use Morgan for HTTP request logging
 app.use(logger('dev'));
-// parse json
+// Parse json
 app.use(jsonParser());
 
-// serve static files from the "public" folder
+// Serve static files from the "public" folder
 app.use('/', express.static('public'));
 
-// app.use('/api', routes);
+// Require and use routes
+var routes = require('./routes');
 app.use('/api', routes.course);
 app.use('/api', routes.review);
 app.use('/api', routes.user);
 
 
 // Add a middleware function to catch 404 errors and forward an error to the global error handler.
-// catch 404 & forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// Adds a global error handler middleware function that writes error information to the response in the JSON format.
+// Add a global error handler middleware function that writes error information to the response in the JSON format.
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({
@@ -55,7 +50,7 @@ app.use(function (err, req, res, next) {
   });
 });
 
-// start listening on our port
+// Start listening on our port
 var server = app.listen(app.get('port'), function() {
   console.log('Express server is listening on port ' + server.address().port);
 });
