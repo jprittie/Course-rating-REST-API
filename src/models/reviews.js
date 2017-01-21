bf5094cd0dc4a60679a4e5ecdd723ee6cd6f7f84
jprittie <eyewallop@gmail.com>
2017-01-21 17:13:35 -0500

    Updates comments

--- src/models/reviews.js
+++ src/models/reviews.js
@@ -1,29 +1,25 @@
 'use strict';
 var mongoose = require('mongoose');
-// Patrick used integer validator here
 
-// not sure I need the following line
-var User = require('./users');
 
 var ReviewSchema = new mongoose.Schema({
 
   user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User'
-    },
-    postedOn: {
-      type: Date,
-      default: Date.now
-    },
-    rating: {
-      type: Number,
-      required: [true, 'Please enter a rating'],
-      min: 1,
-      max: 5,
-      default: 0,
-      // integer: 'Rating must be an integer.'
-    },
-    review: String
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  postedOn: {
+    type: Date,
+    default: Date.now
+  },
+  rating: {
+    type: Number,
+    required: [true, 'Please enter a rating'],
+    min: 1,
+    max: 5,
+    default: 0,
+  },
+  review: String
 
 });
 
