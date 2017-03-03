19ad13ed4b9fb9755689d37ec7b3ad64fbb5e1f2
jprittie <eyewallop@gmail.com>
2017-03-03 16:52:21 -0500

    Updates comments

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.set('port', process.env.PORT || 5000);
 
 // Use Morgan for HTTP request logging
 app.use(logger('dev'));
-// Parse json
+// Parse JSON
 app.use(jsonParser());
 
 // Serve static files from the "public" folder
@@ -33,14 +33,14 @@ app.use('/api', routes.review);
 app.use('/api', routes.user);
 
 
-// Add a middleware function to catch 404 errors and forward an error to the global error handler.
+// Add a middleware function to catch 404 errors and forward an error to the global error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// Add a global error handler middleware function that writes error information to the response in the JSON format.
+// Add a global error handler middleware function that writes error information to the response in JSON format
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({
