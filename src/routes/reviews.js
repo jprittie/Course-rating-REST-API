de98b7881ffe517e90ce489c551178120a4f1ca3
jprittie <eyewallop@gmail.com>
2017-03-03 22:03:20 -0500

    Limits number of reviews per user

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -13,7 +13,6 @@ router.post('/courses/:courseId/reviews', auth, function (req, res, next) {
 
   // Create new review with req.body
   var review = new Review(req.body);
-  // Gets user from auth module
   review.user = req.user;
 
   Course.findById(req.params.courseId)
@@ -28,14 +27,43 @@ router.post('/courses/:courseId/reviews', auth, function (req, res, next) {
     })
     .exec(function(err, course) {
       if (err) return next(err);
+      // console.log(course);
+      console.log(req.user._id);
+
+      // Don't allow more than one review per user
+      for (var i=0; i<course.reviews.length; i++) {
+        if (course.reviews[i].user._id.toJSON() === req.user._id.toJSON()) {
+          err = new Error("Sorry, you can only add one review per course.");
+          err.status = 401;
+          return next(err);
+        }
+      }
+
+      // Don't allow the course owner to post a review on their own course
+      if (req.user._id.toJSON() === course.user._id.toJSON()) {
+        err = new Error("Sorry, you can't review your own courses.");
+        err.status = 401;
+        return next(err);
+      }
+
+
       course.reviews.push(review);
       // Then save the course
       course.save(function (err) {
-      // If error pass to error handler
-        if (err) return next(err);
+        if (err) {
+          // Check for validation errors
+          if (err.name === 'ValidationError') {
+            return res.status(400).json({
+              message: 'Validation Failed', errors: { property: [ { code: 400, message: err.errors.rating.message } ] }
+            });
+          } else {
+            // Send error to middleware error handler
+            return next(err);
+          }
+        }
       });
       // Then save the review
-      review.save(function (err) {
+      review.save(function(err) {
         if (err) {
           // Check for validation errors
           if (err.name === 'ValidationError') {
@@ -61,45 +89,53 @@ router.post('/courses/:courseId/reviews', auth, function (req, res, next) {
 // DELETE /api/courses/:courseId/reviews/:id 204
 // Deletes the specified review and returns no content
 router.delete('/courses/:courseId/reviews/:id', auth, function (req, res, next) {
-  // Remove the review with
-  Review.remove({_id: req.params.id}, function (err) {
-    // If error send to error handler
+
+  Review.findById(req.params.id)
+    .populate('user')
+    .exec(function (err, review) {
+    // If error send to middleware error handler
     if (err) return next(err);
+
+    Course.findById(req.params.courseId)
+      .populate('user')
+      .populate({
+        path: 'reviews',
+        model: 'Review',
+        populate: {
+          path: 'user',
+          model: 'User'
+        }
+      })
+      .exec(function(err, course) {
+        if (err) return next(err);
+
+        // Get current user, course owner and review owner
+        var currentUser = req.user._id.toJSON();
+        var courseOwner = course.user._id.toJSON();
+        var reviewOwner = review.user._id.toJSON();
+
+        // Only the review's user or course owner can delete a review
+        if (currentUser === courseOwner || currentUser === reviewOwner) {
+
+          // Remove the review.
+          Review.findById(req.params.id)
+            .remove()
+            .exec(function(err) {
+              if (err) return next(err);
+            });
+
+          return res.sendStatus(204);
+
+        } else {
+          var error = new Error('Sorry, only the review owner or course owner can delete a review.');
+          res.sendStatus(401);
+          return next(error);
+        }
+
+      }); // Ends exec query
+
   });
 
-  Course.findById(req.params.courseId)
-    .populate('user')
-    .populate({
-      path: 'reviews',
-      model: 'Review',
-      populate: {
-        path: 'user',
-        model: 'User'
-      }
-    })
-    .exec(function(err, course) {
-      if (err) return next(err);
-
-      // Get current user, course owner and review owner
-      var currentUser = req.user._id.toJSON();
-      var courseOwner = course.user._id.toJSON();
-      var reviewOwner = review.user._id.toJSON();
-
-      // Only the review's user or course owner can delete a review
-      if (currentUser === courseOwner || currentUser === reviewOwner) {
-        // Splice out the deleted review from course.reviews array
-        course.reviews.splice(course.reviews.indexOf(req.params.id), 1);
-        // Save the course
-        course.save(function (err) {
-          // If error send to error handler
-          if (err) return next(err);
-        });
-      } // Ends if currentUser
-
-    });
-
-  // Send 204 status
-  return res.sendStatus(204);
 });
 
 module.exports = router;
